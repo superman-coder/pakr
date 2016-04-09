35b50f0774e1d891b00ab400131c1f18bfe232e3
Tien Nguyen <thanhtien2302@gmail.com>
2016-04-09 16:59:57 +0700

    [AND-1] Parking list cell

--- pakr/pakr/AppDelegate.swift
+++ pakr/pakr/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = ParkingListController(nibName: "ParkingListController", bundle: nil)
+        window?.makeKeyAndVisible()
         return true
     }
 
--- pakr/pakr/UserInterface/ListParking/ParkingListController.swift
+++ pakr/pakr/UserInterface/ListParking/ParkingListController.swift
@@ -8,10 +8,58 @@ import UIKit
 
 class ParkingListController: UIViewController {
     
-    public var addressService: AddressService!
+    @IBOutlet weak var parkingTableView: UITableView!
+    
+    internal var addressService: AddressService!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addressService.loadAddress()
+        self.initTableView()
+//        addressService.loadAddress()
+    }
+    
+    func initTableView() {
+        self.parkingTableView.registerNib(UINib(nibName: "ParkingResultCell", bundle: nil), forCellReuseIdentifier: "ParkingResultCell")
+        self.parkingTableView.rowHeight = UITableViewAutomaticDimension
+        self.parkingTableView.estimatedRowHeight = 10
+    }
+}
+
+
+
+extension ParkingListController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ParkingResultCell") as! ParkingResultCell
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        /*
+         UITableViewHeaderFooterView *footer = (UITableViewHeaderFooterView *)view;
+         footer.backgroundView.backgroundColor = [UIColor clearColor];
+         */
+        
+        let footer = view as! UITableViewHeaderFooterView
+        footer.backgroundView?.backgroundColor = UIColor.clearColor()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5
     }
 }
