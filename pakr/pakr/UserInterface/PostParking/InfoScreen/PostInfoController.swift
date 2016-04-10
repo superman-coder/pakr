30fe530e2d914cc5dc510b5c51027a5cf6499709
hqthao <thao.huynh@lazada.com>
2016-04-11 01:52:25 +0700

    feature #3 PageViewController

---
+++ pakr/pakr/UserInterface/PostParking/InfoScreen/PostInfoController.swift
@@ -0,0 +1,17 @@
+//
+//  PostInfoController.swift
+//  pakr
+//
+//  Created by Huynh Quang Thao on 4/11/16.
+//  Copyright © 2016 Pakr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostInfoController: BaseViewController {
+    
+    override func viewDidLoad() {
+    
+    }
+}
