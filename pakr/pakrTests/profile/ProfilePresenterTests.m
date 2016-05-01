4025f13d36df633c58e20b43a1753b8e5863a76e
hqthao <thao.huynh@lazada.com>
2016-05-01 21:57:33 +0700

    add test

---
+++ pakr/pakrTests/profile/ProfilePresenterTests.m
@@ -0,0 +1,62 @@
+//
+//  ProfilePresenterTests.m
+//  pakr
+//
+//  Created by Huynh Quang Thao on 5/1/16.
+//  Copyright © 2016 Pakr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
+@interface ProfilePresenterTests : XCTestCase
+
+@end
+
+
+@implementation ProfilePresenterTests
+
+id profileView;
+id model;
+id router;
+id tracker;
+id adapter;
+id presenter;
+
+- (void)setUp {
+    [super setUp];
+//    model = OCMClassMock([ProfileDataModelImpl class]);
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testInitView {
+}
+
+- (void)testGetHeaderUser {
+    
+}
+
+- (void)testProfileItemClick_AllPostParking {
+    
+}
+
+- (void)testProfileItemClick_PostScreen {
+    
+}
+- (void)testProfileItemClick_AllBookMarkPage {
+    
+}
+
+- (void)testProfileItemClick_LogOut {
+    
+}
+
+- (void)testPerformanceExample {
+    [self measureBlock:^{
+    }];
+}
+
+@end
